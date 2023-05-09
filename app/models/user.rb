67a00e52c7a03a1d5a77f56789582e48a77048c2
NikJensen97 <npjjensen@hotmail.com>
2023-05-08 22:03:10 -0700

    added password minimum length

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ActiveRecord::Base
   has_secure_password
+  validates :email, uniqueness: { case_sensitive: false }
+  validates :password, length: { minimum: 5 }, if: :password_digest_changed?
 end
