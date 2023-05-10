d55a9a2e8185cbba1c10f8fab42ec30c33e6b302
NikJensen97 <npjjensen@hotmail.com>
2023-05-09 21:38:47 -0700

    cypress/integration/product_details.spec.js

---
+++ cypress/integration/product_details.spec.js
@@ -0,0 +1,12 @@
+describe('product details page', () => {
+
+  it('can visit the homepage', () => {
+    cy.visit('http://localhost:3000/')
+  })
+
+  it("There is products on the page", () => {
+    cy.get(".products article").should("be.visible");
+  });
+
+
+});
